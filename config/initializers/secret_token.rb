dd8899a9817efcb77d068ee92d3e3647869cf218
htscs <web@htscs.com>
2012-01-12 18:31:44 +0100

    Home view with css
    creato le fondamenta del sito. sono tutte strisce '.stripe' con un
    contenitore '.stripe_container' al centro largo 960px. Posso
    sovrapporre quante strisce voglio così cresce il sito.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HtscsFla::Application.config.secret_token = 'dc09276c775f1fb08592b2144dd8a2b849d8daf0d4cbe98067fee7d0978dbe77f541ed85c1cd543fde1fb071443f8b749811e063e9504a0b36f7bf7eb16d9dcf'
